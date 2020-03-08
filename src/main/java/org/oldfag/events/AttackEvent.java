246dccfa3ef53ef86430b3ae8f57672875f02e08
John200410 <43681932+John200410@users.noreply.github.com>
2020-03-08 19:52:15 -0400

    fix x1D's garbage code

--- src/main/java/org/oldfag/events/AttackEvent.java
+++ src/main/java/org/oldfag/events/AttackEvent.java
@@ -1,6 +1,5 @@
 package org.oldfag.events;
 
-
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
@@ -8,30 +7,29 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
 
 /**
- *  @author x1D, 03/08/2020
+ * @author x1D, 03/08/2020
  */
 public class AttackEvent implements Listener {
 
-    private Random rand = new Random();
+	private Random rand = new Random();
 
-    @EventHandler
-    public void onAttack(EntityDamageByEntityEvent event) {
-        if (event.getEntity().getType() == EntityType.PLAYER &&
-                event.getDamager().getType() == EntityType.PLAYER) {
-            Player entity = (Player) event.getEntity();
-            // 10% chance of executing
-            if (rand.nextInt(10) == 0 && entity.getInventory().getItemInMainHand() != null) {
-                Bukkit.getWorld(entity.getLocation().getWorld().getName()).dropItemNaturally(
-                        entity.getLocation(), entity.getInventory().getItemInMainHand());
-                entity.getInventory().remove(entity.getInventory().getItemInMainHand());
-                entity.updateInventory();
-            }
+	@EventHandler
+	public void onAttack(EntityDamageByEntityEvent event) {
+		if(event.getEntity().getType() == EntityType.PLAYER && event.getDamager().getType() == EntityType.PLAYER) {
+			Player entity = (Player) event.getEntity();
+			// 10% chance of executing
+			if(rand.nextInt(10) == 0 && entity.getInventory().getItemInMainHand() != null) {
+				Bukkit.getWorld(entity.getLocation().getWorld().getName()).dropItemNaturally(entity.getLocation(), entity.getInventory().getItemInMainHand());
+				entity.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+				entity.updateInventory();
+			}
 
-        }
-    }
+		}
+	}
 
 }
